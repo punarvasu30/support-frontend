cb92170e8831e43e6bd256dbcf58a66d0f1f20bf
Punarvasu20003 <punarvasuingole72@gmail.com>
2026-01-11 14:44:14 +0530

    changed path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
+import Login from "../pages/Login";
 import Register from "../pages/Register";
 
 function App() {
